b9e38160fcdd04422b8a87f14fbb26c2846a9888
Miguel <migue19sep@gmail.com>
2023-04-22 01:26:44 -0500

    Adelanto informe 2

--- Info 2/Datos.m
+++ Info 2/Datos.m
@@ -1,3 +1,15 @@
+%% Variables normales
 R=0.5;
 cs=0.2;
 cp=2;
+out=simular(R,cs,cp);
+
+
+%% Funciones 
+function out=simular(R,cs,cp)
+out=sim("Modelo_Circuito.slx");
+grid on
+bode(out.bodegraf.values)
+end
+
+
