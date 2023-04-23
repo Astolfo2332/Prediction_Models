6db6edf4db405ab868f0d1824d3c8f842b57e8ba
Miguel <migue19sep@gmail.com>
2023-04-22 22:02:56 -0500

    HERE COMES HOPE

--- Info 2/Datos.m
+++ Info 2/Datos.m
@@ -1,37 +1,13 @@
 %% No me juzgen no sé algebra tampoco
 syms s F Fo cs cp R x1 x
-eqn1=R*s*(x1-x)+cs*x1==Fo;
-eqn2=R*s*(x-x1)+cp*x==F-Fo;
+eqn1=R*s*(x1-x)+(1/cs)*x1==Fo;
+eqn2=R*s*(x-x1)+(1/cp)*x==F-Fo;
 sol=solve([eqn1,eqn2],[x,x1]);
 xsol=sol.x
-
-%%
-den=1/(cp + R*s - (R*s)/(cs + R*s))
-num
 %% Variables normales
 R=0.5;
 cs=0.2;
 cp=2;
-%%
-% Ecuación porque no sé sumar
-%Por mallas
-syms x1(t) x(t) Fo F
-dx1=diff(x1,t);
-dx=diff(x,t);
-eqns=[F-cp*x-Fo-R*(dx-dx1)==0,Fo-cs*x1+R*(dx1-dx)==0];
-sm=dsolve(eqns)
-%Por nodos 
-syms x1(t) x(t) Fo F
-dx1=diff(x1,t);
-dx=diff(x,t);
-dFo=diff(Fo,t);
-dF=diff(F,t);
-eqns=[dx==(dF-dFo)/cp,dx-dx1==(F-Fo)/R,dx1==dF/cs];
-sn=dsolve(eqns)
-%% Que la funcion de transferencia da como?
-s=tf("s");
-Ft=1/(s*R-(s*R)/(s*R+cs)+cp)
-Fot=(((s*R+cs)+1)/(s*R+cs))*Ft
 
 %% Simular
 t=0.1:0.1:20;
