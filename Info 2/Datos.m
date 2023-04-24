7c81ac830f27357e7246ec560f577934ec33aa1d
Miguel <migue19sep@gmail.com>
2023-04-24 13:48:56 -0500

    Solucionado lo de las graficas

--- Info 2/Datos.m
+++ Info 2/Datos.m
@@ -16,12 +16,15 @@ Fo=(0<t)*1;
 simin=[t.' F.' Fo.'];
 %% Simular con circuito electrico
 out=simular(R,cs,cp,max(t));
-%% comparar datos
-a=[R cs cp];
-b=[0.2 0.5 1.2];
+%% comparar datos 2 subplots
+a=[0.5 0.2 2];
+b=[0.75 0.314 3.14];
 [out,out2]=comparedata(a,b,10);
 
-
+%% Comparar datos legend
+a=[0.5 0.2 2];
+b=[0.75 0.314 3.14];
+[out,out2]=comparedata2(a,b,15);
 %% Funciones 
 function out=simular(R,cs,cp,time)
 out=sim("Modelo_Circuito.slx","StopTime",num2str(time));
@@ -46,17 +49,13 @@ end
 function [out,out2] =comparedata(a,b,time)
 sub1="R: "+num2str(a(1))+" cs: "+num2str(a(2))+" cp: "+num2str(a(3));
 sub2="R: "+num2str(b(1))+" cs: "+num2str(b(2))+" cp: "+num2str(b(3));
-R=a(1);
-cs=a(2);
-cp=a(3);
+evaluator(a(1),a(2),a(3))
 out=sim("Modelo_Circuito.slx","StopTime",num2str(time));
 figure(1)
 bode(out.bodegraf.values)
 title(["Diagrama de Bode paciente 1",sub1])
 grid on
-R=b(1);
-cs=b(2);
-cp=b(3);
+evaluator(b(1),b(2),b(3))
 out2=sim("Modelo_Circuito.slx","StopTime",num2str(time));
 figure(2)
 bode(out2.bodegraf.values)
@@ -64,7 +63,7 @@ title(["Diagrama de Bode paciente 2",sub2])
 grid on
 x=out.simout.signals.values(:,1);
 y=out.simout.time;
-figure(3)
+figure()
 subplot(2,1,1)
 plot(y,x)
 title("Respuesta x paciente")
@@ -73,10 +72,47 @@ xlabel("tiempo(s)")
 ylabel("Desplazamiento (m)")
 subplot(2,1,2)
 x2=out2.simout.signals.values(:,1);
-plot(y,x2)
+y2=out2.simout.time;
+plot(y2,x2)
 title("Respuesta x paciente")
 subtitle(sub2)
 xlabel("tiempo(s)")
 ylabel("Desplazamiento (m)")
 end
-
+function [out,out2] =comparedata2(a,b,time)
+sub1="R: "+num2str(a(1))+" cs: "+num2str(a(2))+" cp: "+num2str(a(3));
+sub2="R: "+num2str(b(1))+" cs: "+num2str(b(2))+" cp: "+num2str(b(3));
+evaluator(a(1),a(2),a(3))
+out=sim("Modelo_Circuito.slx","StopTime",num2str(time));
+figure(1)
+bode(out.bodegraf.values)
+title(["Diagrama de Bode paciente 1",sub1])
+grid on
+evaluator(b(1),b(2),b(3))
+out2=sim("Modelo_Circuito.slx","StopTime",num2str(time));
+figure(2)
+bode(out2.bodegraf.values)
+title(["Diagrama de Bode paciente 2",sub2])
+grid on
+x=out.simout.signals.values(:,1);
+y=out.simout.time;
+figure()
+plot(y,x)
+hold on
+title("Respuesta x paciente")
+xlabel("tiempo(s)")
+ylabel("Desplazamiento (m)")
+x2=out2.simout.signals.values(:,1);
+y2=out2.simout.time;
+plot(y2,x2)
+title("Respuesta x paciente")
+legend(sub1,sub2)
+end
+function evaluator(R,cs,cp)
+R=num2str(R);
+cs=num2str(cs);
+cp=num2str(cp);
+evalin("base","R="+R+";");
+evalin("base","cs="+cs+";");
+evalin("base","cp="+cp+";");
+end
